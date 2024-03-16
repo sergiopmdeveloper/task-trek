473fe59dc93260ecedd24769c8233742cc4df623
Sergio <sergio.pm.developer@gmail.com>
2024-03-16 19:27:05 +0000

    style: modify styles of the Submit component

--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useFormStatus } from 'react-dom'
+import { cn } from '@/lib/utils'
+import { LoaderCircle } from 'lucide-react'
 
 /**
  * Submit component.
@@ -11,11 +13,20 @@ export default function Submit() {
 
 	return (
 		<button
-			className="rounded bg-theme-yellow px-1.5 py-1 text-theme-white hover:brightness-75"
+			className={cn(
+				'flex items-center justify-center gap-1.5 rounded bg-theme-yellow px-1.5 py-1 text-theme-white',
+				{
+					'hover:brightness-75': !pending,
+					'cursor-not-allowed brightness-75': pending,
+				},
+			)}
 			type="submit"
 			disabled={pending}
 		>
 			{pending ? 'Sending...' : 'Send'}
+			{pending && (
+				<LoaderCircle className="animate-spin stroke-theme-white" size={16} />
+			)}
 		</button>
 	)
 }
