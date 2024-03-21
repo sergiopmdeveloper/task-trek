a18b6c3b41d682a55f55dcbebf5f837f78117396
Sergio <sergio.pm.developer@gmail.com>
2024-03-21 00:20:06 +0000

    refactor: isolate refresh token in custom hook

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,25 +1,13 @@
 'use client'
 
-import refreshToken from '@/actions/refresh-token'
-import { useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import useRefreshToken from '@/hooks/useRefreshToken'
 
 /**
  * User page component.
  * @returns The page component.
  */
 export default function Page() {
-	const searchParams = useSearchParams()
-
-	useEffect(() => {
-		async function refreshTokenWrapper() {
-			await refreshToken()
-		}
-
-		if (!searchParams.get('recentToken')) {
-			refreshTokenWrapper()
-		}
-	}, [searchParams])
+	useRefreshToken()
 
 	return (
 		<main>
