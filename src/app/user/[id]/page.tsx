3f5e99f9810f896f676f81ec2b731f732cf0d6ce
Sergio <sergio.pm.developer@gmail.com>
2024-03-21 20:56:47 +0000

    refactor: remove refresh token feature

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,14 +1,8 @@
-'use client'
-
-import useRefreshToken from '@/hooks/useRefreshToken'
-
 /**
  * User page component.
  * @returns The page component.
  */
 export default function Page() {
-	useRefreshToken()
-
 	return (
 		<main>
 			<h1>User page</h1>
