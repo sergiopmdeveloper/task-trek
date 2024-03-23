f2041c72c677e418e24c0edd6a99f3ca7020ae86
Sergio <sergio.pm.developer@gmail.com>
2024-03-23 19:05:30 +0000

    feat: implement UI of the tasks bar

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -4,6 +4,7 @@ import signOut from '@/actions/sign-out'
 import Header from '@/components/Header'
 import Submit from '@/components/Submit'
 import useUser from '@/hooks/useUser'
+import { Plus } from 'lucide-react'
 
 /**
  * User page component.
@@ -27,6 +28,14 @@ export default function Page({
 						<Submit type="destructive">Sign out</Submit>
 					</form>
 				</Header>
+				<div className="mt-4 h-16 w-full px-4">
+					<div className="mx-auto flex h-full w-full max-w-screen-xl items-center justify-between rounded-xl bg-theme-yellow px-4">
+						<h1 className="text-lg font-semibold text-theme-white">Tasks</h1>
+						<div className="flex h-7 w-7 cursor-pointer items-center justify-center rounded bg-theme-gray hover:brightness-75">
+							<Plus className="h-6 w-6 stroke-theme-black" />
+						</div>
+					</div>
+				</div>
 			</main>
 		)
 	}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import React from 'react'
  */
 export default function Header({ children }: HeaderProps) {
 	return (
-		<header className="my-4 h-16 w-full px-4">
+		<header className="mt-4 h-16 w-full px-4">
 			<div className="mx-auto flex h-full max-w-screen-xl items-center justify-between rounded-xl bg-theme-black px-4 shadow-lg">
 				{children}
 			</div>
